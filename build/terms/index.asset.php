7f97bb3f1424e070d82373fe56a4614a0507725e
aneeshyder <anishcharoliya@gmail.com>
2023-02-11 14:28:43 +0530

    update 1.1

--- build/terms/index.asset.php
+++ build/terms/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e6fec6c5bbfabda09e52');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '83b985f31243be3ffbbd');
